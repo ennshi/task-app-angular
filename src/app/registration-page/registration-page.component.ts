aecd1c492fb4b0bd159b0e62bb3b5b94de605d62
ennshi <enn.shi@gmail.com>
2020-02-09 02:09:23 +0100

    Registration form added

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../shared/interfaces/user';
 
 @Component({
   selector: 'app-registration-page',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-page.component.scss']
 })
 export class RegistrationPageComponent implements OnInit {
-
+  form: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(9), Validators.maxLength(20)])
+    });
   }
-
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+    const user: User = {
+      name: this.form.value.name,
+      email: this.form.value.email,
+      password: this.form.value.password
+    };
+    console.log(user);
+  }
 }
